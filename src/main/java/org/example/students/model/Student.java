c2299dcb816deed6d66b134c36ed838613f42fe7
yonatanfarhi20 <yonatanfarhi1@gmail.com~>
2026-06-30 18:19:46 +0300

    with model

---
+++ src/main/java/org/example/students/model/Student.java
@@ -0,0 +1,34 @@
+package org.example.students.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "students")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Size(max = 30)
+    @Column(length = 30)
+    private String username;
+
+    @Min(18)
+    @Max(60)
+    private int age;
+}
