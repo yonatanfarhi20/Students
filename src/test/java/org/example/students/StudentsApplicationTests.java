75a4f705bad2a9728265f9f3621c8a3f362cf809
yonatanfarhi20 <yonatanfarhi1@gmail.com~>
2026-06-30 18:04:03 +0300

    first commit

---
+++ src/test/java/org/example/students/StudentsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.students;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
